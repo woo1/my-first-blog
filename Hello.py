cb4d5aa006285d3df752073c407209b81fb779e0
wijang's mac <projwi@gmail.com>
2017-01-28 16:14:18 +0900

    My Django Girls, 첫번째 커밋

---
+++ Hello.py
@@ -0,0 +1,21 @@
+
+# name = 'Soonja'
+# if(name == 'Elsa'):
+#         print("Hello EE")
+# elif(name == 'Soonja'):
+#     print("Hello SS")
+# else:
+#     print("Bye")
+
+def hi(name):
+    print("Hi "+name+"!")
+    # print("How about you?")
+
+# hi("Soojan")
+
+girls = ["Sooji", "Sejeong", "Sejin", "Sunny"]
+# for name in girls :
+#     hi(name)
+#     print("Next")
+for i in range(1, 6) : #1~5
+    print(i)
